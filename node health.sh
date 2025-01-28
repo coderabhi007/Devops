36fc449241e21b48558920bc32eaa8605df15103
coderabhi007 <124509430+coderabhi007@users.noreply.github.com>
2025-01-28 15:48:19 +0530

    Create node health.sh

---
+++ node health.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#################
+#Author:Abhishek
+#
+#
+#this script outputs the node health
+#
+#version:v1
+#
+################
+
+df -h
+#-h stands for human redatbel
+free -g
+#-g for ggigabite
+
+nproc
